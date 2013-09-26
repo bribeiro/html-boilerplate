6802db3f05d5b647d8e47749066352dae584cb1a
Greg Nettles <gregnettles@gmail.com>
2013-09-26 15:35:06 -0700

    update require config with Marionette and Handlebars settings

--- static/js/config.js
+++ static/js/config.js
@@ -1,24 +1,54 @@
 /*
-	the projects require.js config,
-	used in build process also
-	see: http://requirejs.org/docs/api.html#config
+    the projects require.js config,
+    used in build process also
+    see: http://requirejs.org/docs/api.html#config
 */
 
 /*global requirejs*/
 requirejs.config({
-	//tell require.js what global this lib makes
-	shim: {
-		'underscore': { exports: '_' },
-		'backbone': { deps: ['underscore','jquery'], exports: 'Backbone' },
-		'modernizr': { exports: 'Modernizr' }
-	},
-	//re-route libs to top-level
-	paths: {
-		'jquery': 'libs/jquery',
-		'backbone': 'libs/backbone',
-		'underscore': 'libs/underscore',
-		'text': 'libs/text',
-		'domReady': 'libs/domReady',
-		'modernizr': 'libs/modernizr'
-	}
+
+    baseUrl:"./static/js/app",
+
+    // 3rd party script alias names
+    paths: {
+        // Core Libraries
+        'jquery': 'libs/jquery',
+        'underscore': 'libs/lodash.underscore',
+        'backbone': 'libs/backbone',
+        'marionette': 'libs/backbone.marionette',
+        "handlebars": "libs/handlebars",
+        'hbs': 'libs/hbs.js',
+        'i18nprecompile': 'libs/i18nprecompile.js',
+
+        // Plugins
+        'text': 'libs/plugins/text',
+        'domReady': 'libs/plugins/domReady',
+        'modernizr': 'libs/modernizr'
+    },
+
+    // Sets the configuration for third party scripts that are not AMD compatible
+    shim: {
+        'backbone': {
+            'deps': ['underscore', 'jquery'],
+            'exports': 'Backbone'
+        },
+        'marionette':{
+            'deps': ['underscore', 'backbone', 'jquery'],
+            'exports': 'Marionette'
+        },
+        'modernizr': {
+            'exports': 'Modernizr'
+        },
+        'handlebars':{
+            'exports': 'Handlebars'
+        }
+    },
+
+    // hbs config
+    hbs : {
+        templateExtension: "html",
+        disableHelpers: true,
+        i18nDirectory: "templates/i18n/"
+    }
+
 });
