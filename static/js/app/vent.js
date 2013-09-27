ad975aec56d8cc9d214964f842023a6aa67a6a4f
Greg Nettles <gregnettles@gmail.com>
2013-09-26 18:03:17 -0700

    usage example for vent

--- static/js/app/vent.js
+++ static/js/app/vent.js
@@ -4,6 +4,12 @@
     Global event aggregator that doesn't explicitely
     depend on the application and can be depended
     on anywhere.
+
+    Usage:
+    define(['vent'], function(vent) {
+        vent.on('eventName', function(){});
+        vent.trigger('eventName');
+    });
 */
 define(['backbone.wreqr'],function(Wreqr){
   return new Wreqr.EventAggregator();
