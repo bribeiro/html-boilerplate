3d8b71a83a8de31e869b4161018e993b1d29964b
Greg Nettles <gregnettles@gmail.com>
2013-09-27 12:25:17 -0700

    remove log

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,6 @@
 require(['./config'], function(){
 	require(['app/app', 'app/routers/AppRouter', 'app/controllers/Controller'], function( app, AppRouter, Controller ){
         // Attach the router and controller
-        console.log(app);
         app.appRouter = new AppRouter({
             controller: new Controller()
         });
